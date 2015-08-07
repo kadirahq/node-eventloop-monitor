a2b068b78638e9ba34b9d1a5d24e24b1db7bc163
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-08-07 21:56:59 +0530

    Add client side support

---
+++ test/index.js
@@ -0,0 +1,36 @@
+var EventloopMonitor = require('../');
+var assert = require('assert');
+
+suite('eventloop-monitor', function() {
+  test('basic usage', function(done) {
+    var monitor = new EventloopMonitor(100);
+    monitor.start();
+    setTimeout(function() {
+      var status = monitor.status();
+      assert.equal(status.pctBlock > 0, true);
+      monitor.stop();
+      done();
+    }, 300);  
+  });
+
+  test('usage just after created', function(done) {
+    var monitor = new EventloopMonitor(100);
+    monitor.start();
+    var status = monitor.status();
+    assert.equal(status.pctBlock === 0, true);
+    monitor.stop();
+    done();
+  });
+
+   test('usage after stopped', function(done) {
+    var monitor = new EventloopMonitor(100);
+    monitor.start();
+
+    setTimeout(function() {
+      assert.equal(monitor.status().pctBlock > 0, true);
+      monitor.stop();
+      assert.equal(monitor.status().pctBlock === 0, true);
+      done();
+    }, 200)
+  });
+});
